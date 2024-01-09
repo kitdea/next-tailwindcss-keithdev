b3315f4c3286fc42fa32bd6f13992008ab537202
kitdea <francismarrosales@gmail.com>
2024-01-09 17:17:04 +0800

    fixed font size for mobile and tablet

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -9,47 +9,47 @@ const Work = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="container text-white m-auto flex flex-col align-middle justify-center h-[80vh] bg-fixed p-4">
+      <div className="container text-white m-auto flex flex-col align-middle justify-center h-full lg:h-[80vh] bg-fixed p-4">
         <div>
-          <h1 className='text-7xl md:text-7xl font-bold mb-10'>Project</h1>
+          <h1 className='text-4xl md:text-5xl lg:text-7xl font-bold mb-10'>Project</h1>
         </div>
         
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           <article className="flex max-w-xl flex-row items-start justify-between border border-gray-500 p-5 rounded-xl">
           
             <div className="group relative">
-              <h3 className="mt-3 text-3xl font-semibold leading-6 text-white group-hover:text-gray-2  00">
+              <h3 className="mt-3 text-xl md:text-3xl font-semibold leading-6 text-white group-hover:text-gray-2  00">
                 <a href="#">
                   <span className="absolute inset-0"></span>
                   Project 1
                 </a>
               </h3>
-              <p className="mt-5 text-lg leading-6 text-white line-clamp-3">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+              <p className="mt-5 text-base md:text-lg leading-6 text-white line-clamp-3">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
             </div>
             
           </article>
           <article className="flex max-w-xl flex-row items-start justify-between border border-gray-500 p-5 rounded-xl">
 
             <div className="group relative">
-              <h3 className="mt-3 text-3xl font-semibold leading-6 text-white group-hover:text-gray-2  00">
+              <h3 className="mt-3 text-xl md:text-3xl font-semibold leading-6 text-white group-hover:text-gray-2  00">
                 <a href="#">
                   <span className="absolute inset-0"></span>
                   Project 2
                 </a>
               </h3>
-              <p className="mt-5 text-lg leading-6 text-white line-clamp-3">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+              <p className="mt-5 text-base md:text-lg leading-6 text-white line-clamp-3">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
             </div>
             
           </article>
           <article className="flex max-w-xl flex-row items-start justify-between border border-gray-500 p-5 rounded-xl">
             <div className="group relative">
-              <h3 className="mt-3 text-3xl font-semibold leading-6 text-white group-hover:text-gray-2  00">
+              <h3 className="mt-3 text-xl md:text-3xl font-semibold leading-6 text-white group-hover:text-gray-2  00">
                 <a href="#">
                   <span className="absolute inset-0"></span>
                   Project 3
                 </a>
               </h3>
-              <p className="mt-5 text-lg leading-6 text-white line-clamp-3">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+              <p className="mt-5 tex-base md:text-lg leading-6 text-white line-clamp-3">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
             </div>
           </article>
         

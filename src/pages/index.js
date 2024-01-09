823d66ae5d04751e8ae019d5ada19ff7c38a4ba2
kitdea <francismarrosales@gmail.com>
2024-01-10 01:24:51 +0800

    changed favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ function HomePage() {
           <title>Web Developer | Web Designer | SEO in the Philippines</title>
           <meta name='description' content='Web Developer Web Designer SEO Philippines' />
           
-          <link rel='icon' href='/favicon.ico' />
+          <link rel='icon' href='/avatar-purple.svg' />
         </Head>
         <Hero headingTitle='Hi I&#39;m Francis' message='#WebDeveloper #WebDesigner #SEO | based in the Philippines' />
     </div>

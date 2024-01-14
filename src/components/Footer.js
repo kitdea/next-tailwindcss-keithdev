a9df824e9c4b8aebcb9d7daa885e8609792de7a1
kitdea <francismarrosales@gmail.com>
2024-01-14 13:36:21 +0800

    fixed the social media link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <div className="text-center text-white">
       <div className='flex justify-center align-middle'>
-        <div className='mx-3 text-2xl'><a href=""><i className="fa-brands fa-linkedin-in"></i></a></div>
-        <div className='mx-3 text-2xl'><a href=""><i className="fa-brands fa-github"></i></a></div>
-        <div className='mx-3 text-2xl'><a href=""><i className="fa-brands fa-facebook-f"></i></a></div>
+        <div className='mx-3 text-2xl'><a href="https://www.linkedin.com/in/francis-mar-rosales/" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin-in"></i></a></div>
+        <div className='mx-3 text-2xl'><a href="https://github.com/kitdea" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a></div>
+        <div className='mx-3 text-2xl'><a href="https://www.facebook.com/keith.rosales/" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook-f"></i></a></div>
       </div>
-      <div className='text-center p-4 text-sm'>
+      <div className='text-center p-4 text-xs md:text-sm'>
         <p>2023 Francis Mar Rosales @ All Rights Reserved</p>
       </div>
     </div>
